2001659ff60e3ca47f3570c66d7b34197657ea31
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2006-09-13 12:20:31 +0000

    I have done it - the python modules now compile and work via cmake on redhat. The problem was all caused by a missing symbolic link! Redhat had libboost_python.so.1, but did not have the symbolic link to libboost_python.so
    This meant that the linker linked each python module to libboost_python.a - the static library! This caused a headache, as it probably screwed up the registry of types across the modules. While I had resolved most of the problems by separating the wrappers from the module definition, it didn't solve everything.
    
    My solution (or should I say hack!) is to add the symbolic link from /usr/lib/libboost_python.so.1 to /usr/lib/libboost_python.so manually, and to then add a configure test to the CMakeLists.txt file that checks to see if the shared library has been found (if not, it dies with an error).

--- branches/devel/build/cmake/FindBoost.cmake
+++ branches/devel/build/cmake/FindBoost.cmake
@@ -1,10 +1,10 @@
 # - Find the Boost includes and libraries.
 # The following variables are set if Boost is found.  If Boost is not
-# found, Boost_FOUND is set to false.
-#  Boost_FOUND        - True when the Boost include directory is found.
-#  Boost_INCLUDE_DIRS - the path to where the boost include files are.
-#  Boost_LIBRARY_DIRS - The path to where the boost library files are.
-#  Boost_LIB_DIAGNOSTIC_DEFINITIONS - Only set if using Windows.
+# found, BOOST_FOUND is set to false.
+#  BOOST_FOUND        - True when the Boost include directory is found.
+#  BOOST_INCLUDE_DIRS - the path to where the boost include files are.
+#  BOOST_LIBRARY_DIRS - The path to where the boost library files are.
+#  BOOST_LIB_DIAGNOSTIC_DEFINITIONS - Only set if using Windows.
 
 # ----------------------------------------------------------------------------
 # If you have installed Boost in a non-standard location or you have
@@ -14,9 +14,9 @@
 # If you have put boost in C:\development\Boost then <Your Path> is
 # "C:/development/Boost" and in this directory there will be two
 # directories called "include" and "lib".
-# 1) After CMake runs, set Boost_INCLUDE_DIR to <Your Path>/include/boost<-version>
+# 1) After CMake runs, set BOOST_INCLUDE_DIR to <Your Path>/include/boost<-version>
 # 2) Use CMAKE_INCLUDE_PATH to set a path to <Your Path>/include. This will allow FIND_PATH()
-#    to locate Boost_INCLUDE_DIR by utilizing the PATH_SUFFIXES option. e.g.
+#    to locate BOOST_INCLUDE_DIR by utilizing the PATH_SUFFIXES option. e.g.
 #    SET(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} "<Your Path>/include")
 # 3) Set an environment variable called ${BOOST_ROOT} that points to the root of where you have
 #    installed Boost, e.g. <Your Path>. It is assumed that there is at least a subdirectory called
@@ -24,8 +24,8 @@
 #
 # Note:
 #  1) If you are just using the boost headers, then you do not need to use
-#     Boost_LIBRARY_DIRS in your CMakeLists.txt file.
-#  2) If Boost has not been installed, then when setting Boost_LIBRARY_DIRS
+#     BOOST_LIBRARY_DIRS in your CMakeLists.txt file.
+#  2) If Boost has not been installed, then when setting BOOST_LIBRARY_DIRS
 #     the script will look for /lib first and, if this fails, then for /stage/lib.
 #
 # Usage:
@@ -34,8 +34,8 @@
 # # Boost
 # FIND_PACKAGE(Boost)
 # ...
-# INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
-# LINK_DIRECTORIES(${Boost_LIBRARY_DIRS})
+# INCLUDE_DIRECTORIES(${BOOST_INCLUDE_DIRS})
+# LINK_DIRECTORIES(${BOOST_LIBRARY_DIRS})
 #
 # In Windows, we make the assumption that, if the Boost files are installed, the default directory
 # will be C:\boost.
@@ -61,13 +61,13 @@ IF(WIN32)
   # If you want to observe which libraries are being linked against then defining
   # BOOST_LIB_DIAGNOSTIC will cause the auto-linking code to emit a #pragma message each time
   # a library is selected for linking.
-  SET(Boost_LIB_DIAGNOSTIC_DEFINITIONS "-DBOOST_LIB_DIAGNOSTIC")
+  SET(BOOST_LIB_DIAGNOSTIC_DEFINITIONS "-DBOOST_LIB_DIAGNOSTIC")
 ENDIF(WIN32)
 
 
 SET(BOOST_INCLUDE_PATH_DESCRIPTION "directory containing the boost include files. E.g /usr/local/include/boost-1_33_1 or c:\\boost\\include\\boost-1_33_1")
 
-SET(BOOST_DIR_MESSAGE "Set the Boost_INCLUDE_DIR cmake cache entry to the ${BOOST_INCLUDE_PATH_DESCRIPTION}")
+SET(BOOST_DIR_MESSAGE "Set the BOOST_INCLUDE_DIR cmake cache entry to the ${BOOST_INCLUDE_PATH_DESCRIPTION}")
 
 SET(BOOST_DIR_SEARCH $ENV{BOOST_ROOT})
 IF(BOOST_DIR_SEARCH)
@@ -94,7 +94,7 @@ SET(SUFFIX_FOR_PATH
 #
 # Look for an installation.
 #
-FIND_PATH(Boost_INCLUDE_DIR NAMES boost/config.hpp PATH_SUFFIXES ${SUFFIX_FOR_PATH} PATHS
+FIND_PATH(BOOST_INCLUDE_DIR NAMES boost/config.hpp PATH_SUFFIXES ${SUFFIX_FOR_PATH} PATHS
 
   # Look in other places.
   ${BOOST_DIR_SEARCH}
@@ -104,15 +104,15 @@ FIND_PATH(Boost_INCLUDE_DIR NAMES boost/config.hpp PATH_SUFFIXES ${SUFFIX_FOR_PA
 )
 
 # Assume we didn't find it.
-SET(Boost_FOUND 0)
+SET(BOOST_FOUND 0)
 
 # Now try to get the include and library path.
-IF(Boost_INCLUDE_DIR)
+IF(BOOST_INCLUDE_DIR)
 
   # Look for the boost library path.
   # Note that the user may not have installed any libraries
   # so it is quite possible the Boost_LIBRARY_PATH may not exist.
-  SET(Boost_LIBRARY_DIR ${Boost_INCLUDE_DIR})
+  SET(Boost_LIBRARY_DIR ${BOOST_INCLUDE_DIR})
 
   IF("${Boost_LIBRARY_DIR}" MATCHES "boost-[0-9]+")
     GET_FILENAME_COMPONENT(Boost_LIBRARY_DIR ${Boost_LIBRARY_DIR} PATH)
@@ -133,19 +133,19 @@ IF(Boost_INCLUDE_DIR)
     ENDIF(EXISTS "${Boost_LIBRARY_DIR}/stage/lib")
   ENDIF(EXISTS "${Boost_LIBRARY_DIR}/lib")
 
-  IF(EXISTS "${Boost_INCLUDE_DIR}")
-    SET(Boost_INCLUDE_DIRS ${Boost_INCLUDE_DIR})
+  IF(EXISTS "${BOOST_INCLUDE_DIR}")
+    SET(BOOST_INCLUDE_DIRS ${BOOST_INCLUDE_DIR})
     # We have found boost. It is possible that the user has not
-    # compiled any libraries so we set Boost_FOUND to be true here.
-    SET(Boost_FOUND 1)
-  ENDIF(EXISTS "${Boost_INCLUDE_DIR}")
+    # compiled any libraries so we set BOOST_FOUND to be true here.
+    SET(BOOST_FOUND 1)
+  ENDIF(EXISTS "${BOOST_INCLUDE_DIR}")
 
   IF(Boost_LIBRARY_DIR AND EXISTS "${Boost_LIBRARY_DIR}")
-    SET(Boost_LIBRARY_DIRS ${Boost_LIBRARY_DIR})
+    SET(BOOST_LIBRARY_DIRS ${Boost_LIBRARY_DIR})
   ENDIF(Boost_LIBRARY_DIR AND EXISTS "${Boost_LIBRARY_DIR}")
-ENDIF(Boost_INCLUDE_DIR)
+ENDIF(BOOST_INCLUDE_DIR)
 
-IF(NOT Boost_FOUND)
+IF(NOT BOOST_FOUND)
   IF(NOT Boost_FIND_QUIETLY)
     MESSAGE(STATUS "Boost was not found. ${BOOST_DIR_MESSAGE}")
   ELSE(NOT Boost_FIND_QUIETLY)
@@ -153,5 +153,5 @@ IF(NOT Boost_FOUND)
       MESSAGE(FATAL_ERROR "Boost was not found. ${BOOST_DIR_MESSAGE}")
     ENDIF(Boost_FIND_REQUIRED)
   ENDIF(NOT Boost_FIND_QUIETLY)
-ENDIF(NOT Boost_FOUND)
+ENDIF(NOT BOOST_FOUND)
 
