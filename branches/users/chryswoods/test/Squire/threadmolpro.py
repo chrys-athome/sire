3661fa64cbedb49a6c2caceab87b906fc01f92a6
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2007-03-19 10:08:39 +0000

    I've cleaned up some of the python tests - they now show Sire doing some pretty cool things!
    test/speed/water_mc_speed.py  - this is the old test/test.py, with a few cleanings
    test/SireCluster/threadff.py  - this has been fixed, and more forcefields have been added.
                                    This runs three forcefields in parallel!
    test/Squire/threadmolpro.py   - this is a MolproFF version of threadff.py - two Molpro  
                                    instances are run in parallel!
    
    Code compiles, links and runs well

--- branches/users/chryswoods/test/Squire/threadmolpro.py
+++ branches/users/chryswoods/test/Squire/threadmolpro.py
@@ -6,6 +6,7 @@ from Sire.FF import *
 from Sire.MM import *
 from Sire.CAS import *
 from Sire.Cluster import *
+from Sire.Squire import *
 from Sire.Maths import *
 from Sire.Qt import *
 from Sire.Units import *
@@ -32,10 +33,16 @@ space = Cartesian()
 switchfunc = HarmonicSwitchingFunction(80.0)
 switchfunc = HarmonicSwitchingFunction(15.0, 14.5)
 
+molproexe = "../../../../../software/molpro/devel/molpro"
+
 #create a forcefield for the molecules
-cljff = InterCLJFF( space, switchfunc )
-ljff = InterLJFF( space, switchfunc )
-coulff = InterCoulombFF( space, switchfunc )
+molproff1 = MolproFF( space, switchfunc )
+molproff2 = MolproFF( space, switchfunc )
+molproff3 = MolproFF( space, switchfunc )
+
+molproff1.setMolproExe(molproexe)
+molproff2.setMolproExe(molproexe)
+molproff3.setMolproExe(molproexe)
 
 #parametise each molecule and add it to the forcefield
 print "Parametising the molecules..."
@@ -52,65 +59,52 @@ ljs = AtomicLJs( [ LJParameter( 3.15365 * angstrom, \
 
 timer.start()
 for mol in mols:
-
       mol.setProperty( "charges", chgs )
       mol.setProperty( "ljs", ljs )
 
-      cljff.add(mol, {cljff.parameters().coulomb() : "charges",
-                      cljff.parameters().lj() : "ljs"})
-                      
-      coulff.add(mol, {coulff.parameters().coulomb() : "charges"})
-      ljff.add(mol, {ljff.parameters().lj() : "ljs"})
+qm_mol = mols[0]
+mm_mols = mols[1:]
+
+molproff1.addToMM(mm_mols)
+molproff2.addToMM(mm_mols)
+molproff3.addToMM(mm_mols)
+
+molproff1.addToQM(qm_mol)
+molproff2.addToQM(qm_mol)
+molproff3.addToQM(qm_mol)
 
 ms = timer.elapsed()
 print "... took %d ms" % ms
 
+timer.start()
+
 #create a thread processor and calculate the energy in the background
-threadproc1 = FFThreadProcessor(cljff)
-threadproc2 = FFThreadProcessor(ljff)
-threadproc3 = FFThreadProcessor(coulff)
+threadproc1 = FFThreadProcessor(molproff1)
+threadproc2 = FFThreadProcessor(molproff2)
 
 active_threadproc1 = threadproc1.activate()
 active_threadproc2 = threadproc2.activate()
-active_threadproc3 = threadproc3.activate()
 
 print "Starting background calculation..."
 active_threadproc1.recalculateEnergy()
 active_threadproc2.recalculateEnergy()
-active_threadproc3.recalculateEnergy()
 
 print "Off it goes...."
 print "Da de da da da..."
 
 #create an FFProcessor, and place the cljff onto it...
-ffproc1 = FFProcessor(cljff)
-ffproc2 = FFProcessor(ljff)
-ffproc3 = FFProcessor(coulff)
+ffproc1 = FFProcessor(molproff3)
 
 print "Is active?", ffproc1.isActive()
-print "Is active?", ffproc2.isActive()
-print "Is active?", ffproc3.isActive()
 
 active_ffproc1 = ffproc1.activate()
-active_ffproc2 = ffproc2.activate()
-active_ffproc3 = ffproc3.activate()
 
 print "Is active?", ffproc1.isActive()
-print "Is active?", ffproc2.isActive()
-print "Is active?", ffproc3.isActive()
 
 print "MAIN THREAD PROCESS"
-print "Total energy == ",active_ffproc1.energy()
-print "Coulomb == %f, LJ == %f" % ( active_ffproc1.energy(cljff.components().coulomb()), \
-                                    active_ffproc1.energy(cljff.components().lj()) )
-
-print "Total energy == ",active_ffproc2.energy()
-print "Total energy == ",active_ffproc3.energy()
-
-print "BACKGROUND THREAD PROCESS"
 print "Total energy == ",active_threadproc1.energy()
-print "Coulomb == %f, LJ == %f" % ( active_threadproc1.energy(cljff.components().coulomb()), \
-                                    active_threadproc1.energy(cljff.components().lj()) )
-
 print "Total energy == ",active_threadproc2.energy()
-print "Total energy == ",active_threadproc3.energy()
+print "Total energy == ",active_ffproc1.energy()
+
+print "Took %d ms" % timer.elapsed()
+
