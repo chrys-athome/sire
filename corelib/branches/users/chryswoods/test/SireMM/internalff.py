50d0a120b37003b4b4d62e6e995c160927fe436b
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2008-06-30 13:05:33 +0000

    Angle energies also appear to work!
    (see test/SireMM/internalff.py)

--- corelib/branches/users/chryswoods/test/SireMM/internalff.py
+++ corelib/branches/users/chryswoods/test/SireMM/internalff.py
@@ -3,6 +3,7 @@ from Sire.MM import *
 from Sire.FF import *
 from Sire.IO import *
 from Sire.Maths import *
+from Sire.Units import *
 
 tip4p = PDB().read("test/io/water.pdb").moleculeAt(0).molecule()
 
@@ -33,6 +34,8 @@ testSplit(H01, O00)
 testSplit(M03, O00)
 testSplit(H02, O00)
 
+print "\nTesting an internal forcefield...\n"
+
 internalff = InternalFF()
 
 bondfuncs = TwoAtomFunctions(tip4p)
@@ -42,9 +45,18 @@ r = internalff.symbols().bond().r()
 bondfuncs.set( O00.index(), H01.index(), 5 * ( 5.0 - r )**2 )
 bondfuncs.set( O00.index(), H02.index(), 5 * ( 5.0 - r )**2 )
 
-tip4p = tip4p.edit().setProperty( "bond", bondfuncs )
+anglefuncs = ThreeAtomFunctions(tip4p)
+
+theta = internalff.symbols().angle().theta()
+
+anglefuncs.set( H01.index(), O00.index(), H02.index(), 3 * ( (90*degrees).value() - theta )**2 )
+
+tip4p = tip4p.edit().setProperty( "bond", bondfuncs ) \
+                    .setProperty( "angle", anglefuncs ) \
+             .commit()
 
 internalff.add( tip4p )
 
 print internalff.energy()
-
+print internalff.energy( internalff.components().bond() )
+print internalff.energy( internalff.components().angle() )
