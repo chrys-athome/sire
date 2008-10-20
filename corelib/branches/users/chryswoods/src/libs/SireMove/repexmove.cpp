102fed11c6f7d51f7f2ba52a2491b56cc5102c58
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2008-10-20 15:56:49 +0000

    I've written the generic replica exchange test - needs to be compiled and tested
    Code is broken

--- corelib/branches/users/chryswoods/src/libs/SireMove/repexmove.cpp
+++ corelib/branches/users/chryswoods/src/libs/SireMove/repexmove.cpp
@@ -25,3 +25,318 @@
   *  at http://siremol.org
   *
 \*********************************************/
+
+#include "repexmove.h"
+#include "repexreplicas.h"
+#include "replicarunner.h"
+
+#include "SireMPI/mpinodes.h"
+
+#include "SireStream/datastream.h"
+#include "SireStream/shareddatastream.h"
+
+using namespace SireMove;
+using namespace SireMaths;
+using namespace SireStream;
+
+static const RegisterMetaType<RepExMove> r_repexmove;
+
+/** Serialise to a binary datastream */
+QDataStream SIREMOVE_EXPORT &operator<<(QDataStream &ds, const RepExMove &repexmove)
+{
+    writeHeader(ds, r_repexmove, 1);
+    
+    SharedDataStream sds(ds);
+    
+    sds << repexmove.rangenerator << repexmove.naccept
+        << repexmove.nreject;
+        
+    return ds;
+}
+
+/** Extract from a binary datastream */
+QDataStream SIREMOVE_EXPORT &operator>>(QDataStream &ds, RepExMove &repexmove)
+{
+    VersionID v = readHeader(ds, r_repexmove);
+    
+    if (v == 1)
+    {
+        SharedDataStream sds(ds);
+        sds >> repexmove.rangenerator >> repexmove.naccept
+            >> repexmove.nreject;
+    }
+    else
+        throw version_error(v, "1", r_repexmove, CODELOC);
+        
+    return ds;
+}
+
+/** Constructor */
+RepExMove::RepExMove() : naccept(0), nreject(0)
+{}
+
+/** Copy constructor */
+RepExMove::RepExMove(const RepExMove &other)
+          : rangenerator(other.rangenerator),
+            naccept(other.naccept), nreject(other.nreject)
+{}
+
+/** Destructor */
+RepExMove::~RepExMove()
+{}
+
+/** Copy assignment operator */
+RepExMove& RepExMove::operator=(const RepExMove &other)
+{
+    if (this != &other)
+    {
+        rangenerator = other.rangenerator;
+        naccept = other.naccept;
+        nreject = other.nreject;
+    }
+    
+    return *this;
+}
+
+/** Comparison operator */
+bool RepExMove::operator==(const RepExMove &other) const
+{
+    return naccept == other.naccept and
+           nreject == other.nreject and
+           rangenerator == other.rangenerator;
+}
+
+/** Comparison operator */
+bool RepExMove::operator!=(const RepExMove &other) const
+{
+    return naccept != other.naccept or
+           nreject != other.nreject or
+           rangenerator != other.rangenerator;
+}
+
+/** Return the number of attempted replica exchange moves */
+quint32 RepExMove::nAttempted() const
+{
+    return naccept + nreject;
+}
+
+/** Return the number of accepted replica exchange moves */
+quint32 RepExMove::nAccepted() const
+{
+    return naccept;
+}
+
+/** Return the number of rejected replica exchange moves */
+quint32 RepExMove::nRejected() const
+{
+    return nreject;
+}
+
+/** Return the ratio of the number of accepted moves to the 
+    total number of attempted moves */
+double RepExMove::acceptanceRatio() const
+{
+    int ntotal = this->nAttempted();
+    
+    if (ntotal == 0)
+        return 0;
+    else
+        return double(this->nAccepted()) / double(ntotal);
+}
+
+/** Completely clear the move statistics */
+void RepExMove::clearMoveStatistics()
+{
+    naccept = 0;
+    nreject = 0;
+}
+
+/** Set the random number generator used when performing the 
+    replica exchange test */
+void RepExMove::setGenerator(const RanGenerator &generator)
+{
+    rangenerator = generator;
+}
+
+/** Return the random number generator used when performing the
+    replica exchange test */
+const RanGenerator& RepExMove::generator() const
+{
+    return rangenerator;
+}
+
+/** Internal function called to record that a move has been accepted */
+void RepExMove::acceptedMove()
+{
+    ++naccept;
+}
+
+/** Internal function called to record that a move has been rejected */
+void RepExMove::rejectedMove()
+{
+    ++nreject;
+}
+          
+/** Perform a replica exchange swap and test between the passed
+    two replicas */
+bool RepExMove::testAndSwap(RepExReplica &replica_a, RepExReplica &replica_b)
+{
+    //get the ensembles of the two replicas
+    const Ensemble &ensemble_a = replica_a.ensemble();
+    const Ensemble &ensemble_b = replica_b.ensemble();
+    
+    if ( (ensemble_a.isNVT() and ensemble_a.isNVT()) or 
+         (ensemble_b.isNPT() and ensemble_b.isNPT()) )
+    {
+        bool need_pv = (ensemble_a.isNPT() and ensemble_b.isNPT());
+    
+        //get the values of the thermodynamic parameters
+        double beta_a = 1.0 / (k_boltz * ensemble_a.temperature()).value();
+        double beta_b = 1.0 / (k_boltz * ensemble_b.temperature()).value();
+        
+        Pressure p_a(0);
+        Pressure p_b(0);
+        
+        if (need_pv)
+        {
+            p_a = ensemble_a.pressure();
+            p_b = ensemble_b.pressure();
+        }
+        
+        //now get the values of the system properties at their current replica level
+        Energy H_a_i = replica_a.energy();
+        Energy H_b_i = replica_b.energy();
+        
+        Volume V_a_i(0);
+        Volume V_b_i(0);
+        
+        if (need_pv)
+        {
+           V_a_i = replica_a_i.volume();
+           V_b_i = replica_b_i.volume();
+        }
+        
+        //now swap the replicas
+        RepExReplica::swap(replica_a, replica_b);
+        
+        //get the values of the system properties at the new replica level
+        Energy H_a_j = replica_a.energy();
+        Energy H_b_j = replica_b.energy();
+        
+        Volume V_a_j(0);
+        Volume V_b_j(0);
+        
+        if (need_pv)
+        {
+            V_a_j = replica_a.volume();
+            V_b_j = replica_b.volume();
+        }
+        
+        //now calculate delta needed for the Monte Carlo test
+        //
+        //   For derivation see Appendix C of Christopher Woods' thesis
+        //
+        //  delta = beta_b * [ H_b_j - H_b_i + P_b (V_b_j - V_b_i) ] + 
+        //          beta_a * [ H_a_i - H_a_j + P_a (V_a_i - V_a_j) ]
+        
+        double delta = beta_b * ( H_b_j - H_b_i + P_b*(V_b_j - V_b_i) ) +
+                       beta_a * ( H_a_i - H_a_j + P_a*(V_a_i - V_a_j) );
+                       
+        return ( delta > 0 or (std::exp(delta) >= rangenerator.rand()) );
+    }
+    else
+    {
+        throw SireError::incompatible_error( QObject::tr(
+            "There is no available replica exchange test that allows tests between "
+            "replicas with ensembles %1 and %2.")
+                .arg(ensemble0.toString(), ensemble1.toString()), CODELOC );
+                
+    }
+
+    return false;
+}
+          
+/** Perform the replica exchange swaps and accept or reject them
+    according to replica exchange tests */
+void RepExMove::testAndSwap(RepExReplicas &replicas)
+{
+    if (replicas.count() <= 1)  
+        //no pairs to swap
+        return;
+        
+    //do we swap the even pairs or the odd pairs?
+    int start = 0;
+    int nreplicas = replicas.count();
+    
+    if (nreplicas > 2 and rangenerator.randBool())
+    {
+        start = 1;
+    }
+    
+    //loop over all pairs
+    for (int i=start; i<nreplicas; ++i)
+    {
+        RepExReplica replica0 = replicas[i];
+        RepExReplica replica1 = replicas[i+1];
+        
+        if (this->testAndSwap(replica0, replica1))
+        {
+            //save the new replicas
+            replicas.setReplica(i, replica0);
+            replicas.setReplica(i+1, replica1);
+            
+            this->acceptedMove();
+        }
+        else
+            this->rejectedMove();
+    }
+}
+          
+/** Perform 'nmoves' replica exchange moves on the replicas in 'replicas',
+    running all jobs using the passed replica runner, and recording statistics if
+    'record_stats' is true. */
+void RepExMove::move(RepExReplicas &replicas, int nmoves,
+                     const ReplicaRunner &reprunner, bool record_stats)
+{
+    if (nmoves <= 0 or replicas.isEmpty())
+        return;
+
+    RepExMove old_state(*this);
+    boost::shared_ptr<RepExReplicas> old_replicas( replicas.clone() );
+    
+    try
+    {
+        for (int i=0; i<nmoves; ++i)
+        {
+            //run a block of sampling on each replica
+            reprunner.run(replicas, record_stats);
+            
+            //now perform the replica exchange test on the replicas
+            this->testAndSwap(replicas);
+        }
+    }
+    catch(...)
+    {
+        //restore the old state
+        this->operator=(old_state);
+        replicas.assign( *old_replicas );
+        throw;
+    }
+}
+
+/** Perform 'nmoves' replica exchange moves on the replicas in 'replicas',
+    running all jobs in the local thread, and recording statistics if
+    'record_stats' is true. */
+void RepExMove::move(RepExReplicas &replicas, int nmoves, bool record_stats)
+{
+    this->move(replicas, nmoves, BasicRepRunner(), record_stats);
+}
+
+/** Perform 'nmoves' replica exchange moves on the replicas in 'replicas',
+    running all jobs on as many of the nodes in 'nodes' as possible, 
+    and recording statistics if 'record_stats' is true. */
+void RepExMove::move(RepExReplicas &replicas, int nmoves,
+                     const MPINodes &nodes, bool record_stats)
+{
+    this->move(replicas, nmoves, MPIRepRunner(nodes), record_stats);
+}
--- corelib/branches/users/chryswoods/src/libs/SireMove/repexmove.h
+++ corelib/branches/users/chryswoods/src/libs/SireMove/repexmove.h
@@ -26,3 +26,115 @@
   *
 \*********************************************/
 
+#ifndef SIREMOVE_REPEXMOVE_H
+#define SIREMOVE_REPEXMOVE_H
+
+#include "SireMaths/rangenerator.h"
+
+SIRE_BEGIN_HEADER
+
+namespace SireMove
+{
+class RepExMove;
+}
+
+QDataStream& operator<<(QDataStream&, const SireMove::RepExMove&);
+QDataStream& operator>>(QDataStream&, SireMove::RepExMove&);
+
+namespace SireMPI
+{
+class MPINodes;
+}
+
+namespace SireMove
+{
+
+class RepExReplicas;
+class RepExReplica;
+
+class ReplicaRunner;
+
+using SireMaths::RanGenerator;
+using SireMPI::MPINodes;
+
+/** This class is used to perform replica exchange moves on a collection
+    of RepExReplicas. Each move involves running a block of sampling
+    on each of the replicas, and then performing replice exchange swaps
+    and tests between pairs.
+    
+    @author Christopher Woods
+*/
+class SIREMOVE_EXPORT RepExMove
+{
+
+friend QDataStream& ::operator<<(QDataStream&, const RepExMove&);
+friend QDataStream& ::operator>>(QDataStream&, RepExMove&);
+
+public:
+    RepExMove();
+    
+    RepExMove(const RepExMove &other);
+    
+    ~RepExMove();
+    
+    RepExMove& operator=(const RepExMove &other);
+
+    static const char* typeName()
+    {
+        return QMetaType::typeName( qMetaTypeId<RepExMove>() );
+    }
+    
+    const char* what() const
+    {
+        return RepExMove::typeName();
+    }
+
+    bool operator==(const RepExMove &other) const;
+    bool operator!=(const RepExMove &other) const;
+    
+    quint32 nAttempted() const;
+    quint32 nAccepted() const;
+    quint32 nRejected() const;
+    
+    double acceptanceRatio() const;
+    
+    void clearMoveStatistics();
+    
+    void setGenerator(const RanGenerator &generator);
+    const RanGenerator& generator() const;
+
+    void move(RepExReplicas &replicas, int nmoves, bool record_stats=true);
+    
+    void move(RepExReplicas &replicas, int nmoves,
+              const MPINodes &nodes, bool record_stats=true);
+              
+    void move(RepExReplicas &replicas, int nmoves,
+              const ReplicaRunner &reprunner, bool record_stats=true);
+
+protected:
+    void testAndSwap(RepExReplicas &replicas);
+    bool testAndSwap(RepExReplica &replica0, RepExReplica &replica1);
+
+    void acceptedMove();
+    void rejectedMove();
+
+private:
+    /** The random number generator used to accept or reject the moves */
+    RanGenerator rangenerator;
+    
+    /** The number of times a replica exchange move has been accepted */
+    quint32 naccept;
+    
+    /** The number of times a replica exchange move has been rejected */
+    quint32 nreject;
+};
+
+}
+
+Q_DECLARE_METATYPE( SireMove::RepExMove )
+
+SIRE_EXPOSE_CLASS( SireMove::RepExMove )
+
+SIRE_END_HEADER
+
+#endif
