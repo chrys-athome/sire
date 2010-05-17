d7a11bd0828b45be9b2f7e85d3a7d91e06e4f5bd
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2010-05-17 07:54:26 +0000

    I'm now finishing up the dynamics code - this commit updates the dynamics test by running a simulations of a box of neon.

--- corelib/branches/devel/test/SireMove/dynamics.py
+++ corelib/branches/devel/test/SireMove/dynamics.py
@@ -18,112 +18,48 @@ t = QTime()
 
 cljff = InterCLJFF("cljff")
 
-cljff_a = InterCLJFF("cljff_a")
-cljff_b = InterCLJFF("cljff_b")
-cljff_a_b = InterGroupCLJFF("cljff_a_b")
+ne = Molecule("Neon")
+ne = ne.edit().add( CGName("1") ).add( AtomName("Ne") ).molecule().commit()
 
-ffs = [ cljff, cljff_a, cljff_b, cljff_a_b ]
+ne = ne.edit().atom(AtomName("Ne")) \
+              .setProperty("coordinates", Vector(0,0,0)) \
+              .setProperty("element", Element("Neon")) \
+              .setProperty("charge", 0*mod_electron) \
+              .setProperty("LJ", LJParameter.fromRMinAndEpsilon(3.06*angstrom,-0.086*kcal_per_mol)) \
+              .molecule() \
+              .commit()
 
-mincoords = Vector(-18.3854, -18.66855, -18.4445)
-maxcoords = Vector( 18.3854,  18.66855,  18.4445)
+neon = MoleculeGroup("neon")
 
-vol = PeriodicBox(mincoords, maxcoords)
-switchfunc = CHARMMSwitchingFunction(15*angstrom, 14.5*angstrom)
+# create a box of neon
+for i in range(0,6):
+    for j in range(0,6):
+        for k in range(0,6):
+            ne = ne.edit().renumber() \
+                   .setProperty("coordinates", AtomCoords([Vector(i*3.06,j*3.06,k*3.06)]) ) \
+                   .commit()
 
-for ff in ffs:
-    ff.setSpace(vol)
-    ff.setSwitchingFunction(switchfunc)
+            neon.add(ne)
 
-solvent = MoleculeGroup("solvent")
-
-mols = PDB().read("test/io/water.pdb")
-                                                
-print "Read in %d molecules!" % mols.nMolecules()
-
-i = 0
-
-t.start()
-mol = mols.moleculeAt(0).molecule()
-
-mol = mol.edit().atom( AtomName("O00") ) \
-                    .setProperty("LJ", LJParameter(3.15363*angstrom,  \
-                                                   0.15500*kcal_per_mol))\
-                    .setProperty("mass", Element("O").mass() ).molecule() \
-                .atom( AtomName("H01") ) \
-                    .setProperty("charge", 0.520 * mod_electron) \
-                    .setProperty("mass", Element("H").mass() ).molecule() \
-                .atom( AtomName("H02") ) \
-                    .setProperty("charge", 0.520 * mod_electron) \
-                    .setProperty("mass", Element("H").mass() ).molecule() \
-                .atom( AtomName("M03") ) \
-                    .setProperty("charge", -1.04 * mod_electron).molecule() \
-         .commit()
-
-charges = mol.property("charge")
-ljs = mol.property("LJ")
-masses = mol.property("mass")
-
-cljff.add(mol)
-cljff_a.add(mol)
-cljff_a_b.add(mol, MGIdx(0))
-
-solvent.add(mol)
-
-for i in range(1, mols.nMolecules()):
-    mol = mols.moleculeAt(i).molecule()
-
-    mol = mol.edit().rename("T4P") \
-                    .setProperty("charge", charges) \
-                    .setProperty("LJ", ljs) \
-                    .setProperty("mass", masses) \
-             .commit()
-
-    solvent.add(mol)
-
-    cljff.add(mol)
-
-    if (i % 2 == 0):
-        cljff_a.add(mol)
-        cljff_a_b.add( mol, MGIdx(0) )
-    else:
-        cljff_b.add(mol)
-        cljff_a_b.add( mol, MGIdx(1) )
-
-ms = t.elapsed()
-print "Parameterised all of the water molecules (in %d ms)!" % ms
-
-print "Getting the force tables..."
-
-t.start()
-forcetable = ForceTable( solvent )
-
-print "took %d ms" % t.elapsed()
-
-
-print "Calculating the forces..."
-t.start()
-
-cljff.force(forcetable)
-
-print "took %d ms" % t.elapsed()
+cljff = InterCLJFF("neon-neon")
+cljff.add(neon)
 
 system = System()
-system.add(solvent)
+system.add(neon)
 system.add(cljff)
 
 t.start()                                       
 print "Initial energy = %s" % system.energy()
 print "(took %d ms)" % t.elapsed()
-print system.property("space")
-print system.property("switchingFunction")
 
-mdmove = MolecularDynamics( solvent, VelocityVerlet(1*femtosecond) )
+mdmove = MolecularDynamics( neon, VelocityVerlet(1*femtosecond) )
 
 for i in range(0,10):
     print "\nmove %d" % (i+1)
-    mdmove.move(system, 1)
+    mdmove.move(system, 10)
 
     print system.energy()
     print mdmove.kineticEnergy()
     print system.energy() + mdmove.kineticEnergy()
 
+    PDB().write(system.molecules(), "test%0004d.pdb" % i)
