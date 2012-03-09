76168a9e608337bbaad72273ae608af280e8d6e7
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-03-09 08:10:21 +0000

    Made space for the GUI code and fixed some bugs in the other CMakeLists.txt files.

---
+++ software_project/conspire/trunk/GUI/cpp/main.cpp
@@ -0,0 +1,46 @@
+/********************************************\
+  *
+  *  Conspire/GUI
+  *
+  *  Copyright (C) 2012  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors via the developer's mailing list
+  *  at http://siremol.org
+  *
+\*********************************************/
+
+#include "Conspire/GUI/view.h"
+
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char **argv)
+{
+    QApplication a(argc, argv);
+    
+    QMainWindow *m = new QMainWindow(0);
+    m->show();
+    
+    a.exec();
+    
+    delete m;
+    
+    return 0;
+}
