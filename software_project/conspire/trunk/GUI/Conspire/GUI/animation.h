ce276204bced3821f5ee170347aea610933e51ac
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-04-17 17:09:05 +0000

    I've added an Animation class that holds and manages the QAbstractAnimation objects that are used to animate the GUI. This class manages the animation so that the PageView holders can know what is being animated, and can manage user interaction around those animations.

---
+++ software_project/conspire/trunk/GUI/Conspire/GUI/animation.h
@@ -0,0 +1,194 @@
+#ifndef CONSPIRE_ANIMATION_H
+#define CONSPIRE_ANIMATION_H
+/********************************************\
+  *
+  *  Conspire
+  *
+  *  Copyright (C) 2012  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors via the developer's mailing list
+  *  at http://siremol.org
+  *
+\*********************************************/
+
+#include "Conspire/conspire.h"
+
+#include <QObject>
+#include <QPointer>
+
+CONSPIRE_BEGIN_HEADER
+
+class QAbstractAnimation;
+class QTimer;
+
+namespace Conspire
+{
+    class Animation;
+    class PagePointer;
+
+    /** This class holds a safe pointer to an Animation. This
+        pointer will automatically manage the reference count
+        to the Animation and will ensure that the Animation is
+        deleted when it is no longer referenced. In addition,
+        it will also automatically lose the reference if the
+        Animation is deleted outside of this pointer. */
+    class CONSPIRE_EXPORT AnimationPointer : public QObject
+    {
+        Q_OBJECT
+    
+    public:
+        AnimationPointer();
+        AnimationPointer(Animation *animation);
+        
+        AnimationPointer(const AnimationPointer &other);
+        
+        ~AnimationPointer();
+        
+        Animation* data();
+        
+        bool isNull() const;
+        
+        operator Animation*();
+    
+        Animation& operator*() const;
+        Animation* operator->() const;
+        
+        AnimationPointer& operator=(const AnimationPointer &other);
+        AnimationPointer& operator=(Animation *animation);
+        
+        bool operator==(const AnimationPointer &other) const;
+        bool operator!=(const AnimationPointer &other) const;
+
+    private slots:
+        void destroyed(QObject *obj);
+        
+    private:
+        Animation *a;
+    };
+
+    /** This class represents an Animation that will be performed
+        on the GUI. Animations are managed so that they can be
+        stopped and started globally, or disabled if the user
+        doesn't like them, or if they take up too many resources.
+        
+        To run an Animation, a Page will typically create an
+        Animation and then emit a signal that will push the animation
+        onto the PageView that holds the Page. If the Animation
+        is blocking, then the PageView will disable all input to the
+        Pages it controls while the Animation is underway, and will provide a 
+        quick and painless way for the user to kill the Animation
+        and proceeed immediately to the final frame. At the end
+        of the Animation, the PageView will reenable input to the Pages
+        it controls.
+        
+        If the Animation is not blocking, then the Animation will 
+        run in the background without disrupting user input, but will
+        still be killable by the user. If the Animation is modal, then
+        it will go into a queue for the PageView such that only one
+        Animation at a time is in process. Otherwise, the Animation
+        will be performed in parallel.
+        
+        The Animation can be set to have an "end" time, so that,
+        once this "end" time has passed, it will be killed and moved
+        automatically to the end state. This ensures that Animations
+        are not built up, killing the responsiveness of the application.
+        
+        The Animation can retain a PagePointer to the page or pages that created
+        the Animation (and that likely contains the objects being animated).
+        This should ensure that the objects being animated are not
+        deleted before the Animation has completed.
+    
+        Note that you build the Animation using the classes and 
+        capabilities of the Qt Animation framework. You should then
+        pass that animation to this class, and let this class then
+        control starting, stopping and deleting that animation.
+    
+        If you want to run or combine multiple animations together,
+        use the QAnimationGroup class to merge them into a single
+        animation that you can then pass to this class.
+    
+        Note that "stop" means something different for Animation
+        than QAbstractAnimation. For Animation, when you stop the
+        animation, it will automatically advance immediately to
+        the final frame, so that the animation can be forced
+        to jump to the end.
+    
+        @author Christopher Woods
+    */
+    class CONSPIRE_EXPORT Animation : public QObject
+    {
+        Q_OBJECT
+    
+    public:
+        Animation(QObject *parent=0);
+        Animation(QAbstractAnimation *animation, QObject *parent=0);
+        Animation(QAbstractAnimation *animation,
+                  PagePointer page, QObject *parent=0);
+        Animation(QAbstractAnimation *animation,
+                  PagePointer page0, PagePointer page1,
+                  QObject *parent=0);
+                  
+        ~Animation();
+
+        void setAutoStopDelay(int ms);
+        int autoStopDelay() const;
+
+    public slots:
+        void start();
+        void stop();
+
+    protected:
+        friend class AnimationPointer;
+        void incref();
+        bool decref();
+        
+    private:
+        void build();
+    
+        /** Pointer to the animation to be performed */
+        QPointer<QAbstractAnimation> anim;
+        
+        /** Pointer to the timer used to force an animation to end */
+        QPointer<QTimer> stop_timer;
+        
+        /** The pages that contain the items being referenced by the
+            animation. These references are held until the animation
+            has completed, thereby ensuring that the pages won't disappear
+            before the animation is over */
+        QList<PagePointer> pages;
+        
+        /** The delay, in milliseconds, after the expected end time of
+            the animation, at which the animation will be automatically
+            stopped. This is used to prevent animations stacking up
+            and blocking the user-interface when the system is under
+            heavy load. If this is 0 or less, then the animation
+            is either a looping animation, or it will not be automatically 
+            stopped */
+        int autostop_delay;
+
+        /** The number of AnimationPointer pointers currently 
+            pointing to this animation */
+        int ref_count;
+    };
+}
+
+CONSPIRE_END_HEADER
+
+#endif
---
+++ software_project/conspire/trunk/GUI/cpp/animation.cpp
@@ -0,0 +1,319 @@
+/********************************************\
+  *
+  *  Conspire
+  *
+  *  Copyright (C) 2012  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors via the developer's mailing list
+  *  at http://siremol.org
+  *
+\*********************************************/
+
+#include "Conspire/GUI/animation.h"
+#include "Conspire/GUI/page.h"
+
+#include "Conspire/exceptions.h"
+
+#include <QAbstractAnimation>
+#include <QTimer>
+
+using namespace Conspire;
+
+////////
+//////// Implementation of AnimationPointer
+////////
+
+/** Constructor */
+AnimationPointer::AnimationPointer() : QObject(), a(0)
+{}
+
+/** Construct to hold a pointer to 'Animation'. */
+AnimationPointer::AnimationPointer(Animation *animation) : QObject(), a(animation)
+{
+    if (a)
+    {
+        a->incref();
+        connect(a, SIGNAL(destroyed(QObject*)), this, SLOT(destroyed(QObject*)));
+    }
+}
+
+/** Copy constructor */
+AnimationPointer::AnimationPointer(const AnimationPointer &other) : QObject(), a(other.a)
+{
+    if (a)
+    {
+        a->incref();
+        connect(a, SIGNAL(destroyed(QObject*)), this, SLOT(destroyed(QObject*)));
+    }
+}
+
+/** Destructor */
+AnimationPointer::~AnimationPointer()
+{
+    if (a)
+    {
+        if (a->decref())
+        {
+            a->stop();
+            a->deleteLater();
+        }
+    }
+}
+
+/** Return the raw pointer to the Animation */
+Animation* AnimationPointer::data()
+{
+    return a;
+}
+
+/** Return whether or not this pointer is null */
+bool AnimationPointer::isNull() const
+{
+    return a == 0;
+}
+
+/** Return the raw pointer to the Animation */
+AnimationPointer::operator Animation*()
+{
+    return a;
+}
+
+/** Dereference the pointer */
+Animation& AnimationPointer::operator*() const
+{
+    if (not a)
+    {
+        throw Conspire::program_bug( Conspire::tr( 
+                "An attempt has been made to dereference a null AnimationPointer!"),
+                    CODELOC );
+    }
+    
+    return *a;
+}
+
+/** Return the raw pointer to the Animation */
+Animation* AnimationPointer::operator->() const
+{
+    return a;
+}
+
+/** Copy assignment operator */
+AnimationPointer& AnimationPointer::operator=(Animation *animation)
+{
+    if (a != animation)
+    {
+        if (a)
+        {
+            //make sure that we are no longer told when this object is destroyed
+            a->disconnect(this);
+            
+            //decrease the reference count
+            if (a->decref())
+            {
+                a->stop();
+                a->deleteLater();
+            }
+        }
+
+        animation->incref();
+        connect(animation, SIGNAL(destroyed(QObject*)), this, SLOT(destroyed(QObject*)));
+        a = animation;
+    }
+    
+    return *this;
+}
+
+/** Copy assignment operator */
+AnimationPointer& AnimationPointer::operator=(const AnimationPointer &other)
+{
+    return this->operator=(other.a);
+}
+
+/** Comparison operator */
+bool AnimationPointer::operator==(const AnimationPointer &other) const
+{
+    return a == other.a;
+}
+
+/** Comparison operator */
+bool AnimationPointer::operator!=(const AnimationPointer &other) const
+{
+    return a != other.a;
+}
+
+/** Function called when the Animation pointed to by this pointer
+    is destroyed */
+void AnimationPointer::destroyed(QObject *obj)
+{
+    if (obj != a)
+        conspireDebug() << "WEIRD - POINTER MISMATCH" << a << obj;
+        
+    a = 0;
+}
+
+////////
+//////// Implementation of Animation
+////////
+
+void Animation::build()
+{
+    ref_count = 0;
+    autostop_delay = 250;
+}
+
+/** Constructor */
+Animation::Animation(QObject *parent) : QObject(parent)
+{
+    build();
+}
+
+/** Construct, passing in the animation to perform */
+Animation::Animation(QAbstractAnimation *animation, QObject *parent)
+          : QObject(parent), anim(animation)
+{
+    build();
+}
+
+/** Construct, passing in the animation to perform, and the
+    Page on which the animation acts */
+Animation::Animation(QAbstractAnimation *animation,
+                     PagePointer page, QObject *parent)
+          : QObject(parent), anim(animation)
+{
+    pages.append(page);
+}
+
+/** Construct, passing in the animation to perfrom, and the
+    Pages on which the animation acts */
+Animation::Animation(QAbstractAnimation *animation,
+                     PagePointer page0, PagePointer page1,
+                     QObject *parent)
+          : QObject(parent), anim(animation)
+{
+    pages.append(page0);
+    pages.append(page1);
+}
+          
+/** Destructor */
+Animation::~Animation()
+{}
+
+/** Set the delay after the end of the expected end of the 
+    animation at which the animation will be automatically stopped.
+    If any value less than or equal to zero is passed, then
+    the animation will never be automatically stopped. Note that
+    this is ignored if the animation has an undefined duration
+    (e.g. if it runs continuously for ever) */
+void Animation::setAutoStopDelay(int ms)
+{
+    if (ms < 0)
+        autostop_delay = 0;
+    else
+        autostop_delay = ms;
+}
+
+/** Return the delay after which the animation will be automatically
+    stopped. */
+int Animation::autoStopDelay() const
+{
+    return autostop_delay;
+}
+
+/** Start the animation. If it is already running, then it stops
+    the current run, and then restarts it from the beginning. */
+void Animation::start()
+{
+    if (anim)
+    {
+        if (stop_timer or (anim->state() == QAbstractAnimation::Running))
+        {
+            if (stop_timer)
+            {
+                stop_timer->disconnect();
+                stop_timer->deleteLater();
+                stop_timer = 0;
+            }
+            
+            anim->stop();
+        }
+
+        int stop_time = 0;
+        
+        if (anim->duration() > 0 and autostop_delay > 0)
+        {
+            stop_time = anim->duration() + autostop_delay;
+        }
+        
+        anim->start();
+        
+        if (stop_time > 0)
+        {
+            stop_timer = new QTimer(this);
+            stop_timer->setSingleShot(true);
+            connect(stop_timer.data(), SIGNAL(timeout()), this, SLOT(stop()));
+            stop_timer->start();
+        }
+    }
+}
+
+/** Stop the animation. This will automatically advance the animation
+    to the last frame (or the last frame of the loop, if this is a looping
+    animation) */
+void Animation::stop()
+{
+    if (stop_timer)
+    {
+        stop_timer->disconnect();
+        stop_timer->deleteLater();
+        stop_timer = 0;
+    }
+    
+    anim->stop();
+    
+    if (anim->direction() == QAbstractAnimation::Forward)
+    {
+        anim->setCurrentTime(anim->duration());
+    }
+    else
+    {
+        anim->setCurrentTime(0);
+    }
+}
+
+/** Internal function used to increase the reference count */
+void Animation::incref()
+{
+    ref_count += 1;
+}
+
+/** Internal function used to decrease the reference count. This 
+    returns "true" if the reference count has dropped to zero */
+bool Animation::decref()
+{
+    ref_count -= 1;
+    
+    if (ref_count <= 0)
+    {
+        ref_count = 0;
+        return true;
+    }
+    else
+        return false;
+}
