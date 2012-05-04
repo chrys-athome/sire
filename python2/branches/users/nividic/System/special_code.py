f9fbf48b8f9e47dd1edab7aa15a29a75dd3b221e
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-05-04 10:12:03 +0000

    Added a branch of python2 for Gaetano

---
+++ python2/branches/users/nividic/System/special_code.py
@@ -0,0 +1,27 @@
+###############################################
+#
+# This file contains special code to help
+# with the wrapping of SireSystem classes
+#
+#
+
+import sys
+import pickle
+
+from pyplusplus.module_builder import call_policies
+
+sys.path.append("../AutoGenerate")
+from scanheaders import *
+
+def fix_MonitorID(c):
+    c.add_declaration_code( "#include \"systemmonitors.h\"" )
+    c.add_declaration_code( "#include \"monitorname.h\"" )
+
+fix_MonitorName = fix_MonitorID
+fix_MonitorIdx = fix_MonitorID
+
+special_code = { "SireSystem::MonitorID" : fix_MonitorID,
+                 "SireSystem::MonitorName" : fix_MonitorName,
+                 "SireSystem::MonitorIdx" : fix_MonitorIdx
+               }
+
