e032a1c83ba7fd64b81afc724fb0d130fe187b53
chryswoods <chryswoods@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-08-08 11:55:36 +0000

    The python wrappers associated with the new GridFF
    Code is working

--- python2/branches/devel/MM/GridFF.pypp.cpp
+++ python2/branches/devel/MM/GridFF.pypp.cpp
@@ -3,6 +3,7 @@
 // (C) Christopher Woods, GPL >= 2 License
 
 #include "boost/python.hpp"
+#include "Helpers/clone_const_reference.hpp"
 #include "GridFF.pypp.hpp"
 
 namespace bp = boost::python;
@@ -16,6 +17,8 @@ namespace bp = boost::python;
 #include "gridff.h"
 
 #include <QDebug>
+
+#include <QTime>
 
 #include "gridff.h"
 
@@ -33,6 +36,26 @@ void register_GridFF_class(){
         bp::scope GridFF_scope( GridFF_exposer );
         GridFF_exposer.def( bp::init< QString const & >(( bp::arg("name") )) );
         GridFF_exposer.def( bp::init< SireMM::GridFF const & >(( bp::arg("other") )) );
+        { //::SireMM::GridFF::averageDeltaTimeSaved
+        
+            typedef double ( ::SireMM::GridFF::*averageDeltaTimeSaved_function_type )(  ) const;
+            averageDeltaTimeSaved_function_type averageDeltaTimeSaved_function_value( &::SireMM::GridFF::averageDeltaTimeSaved );
+            
+            GridFF_exposer.def( 
+                "averageDeltaTimeSaved"
+                , averageDeltaTimeSaved_function_value );
+        
+        }
+        { //::SireMM::GridFF::averageTotalTimeSaved
+        
+            typedef double ( ::SireMM::GridFF::*averageTotalTimeSaved_function_type )(  ) const;
+            averageTotalTimeSaved_function_type averageTotalTimeSaved_function_value( &::SireMM::GridFF::averageTotalTimeSaved );
+            
+            GridFF_exposer.def( 
+                "averageTotalTimeSaved"
+                , averageTotalTimeSaved_function_value );
+        
+        }
         { //::SireMM::GridFF::buffer
         
             typedef ::SireUnits::Dimension::Length ( ::SireMM::GridFF::*buffer_function_type )(  ) const;
@@ -61,6 +84,28 @@ void register_GridFF_class(){
             GridFF_exposer.def( 
                 "coulombCutoff"
                 , coulombCutoff_function_value );
+        
+        }
+        { //::SireMM::GridFF::deltaCoulombErrorHistogram
+        
+            typedef ::SireMaths::Histogram const & ( ::SireMM::GridFF::*deltaCoulombErrorHistogram_function_type )(  ) const;
+            deltaCoulombErrorHistogram_function_type deltaCoulombErrorHistogram_function_value( &::SireMM::GridFF::deltaCoulombErrorHistogram );
+            
+            GridFF_exposer.def( 
+                "deltaCoulombErrorHistogram"
+                , deltaCoulombErrorHistogram_function_value
+                , bp::return_value_policy<bp::clone_const_reference>() );
+        
+        }
+        { //::SireMM::GridFF::deltaLJErrorHistogram
+        
+            typedef ::SireMaths::Histogram const & ( ::SireMM::GridFF::*deltaLJErrorHistogram_function_type )(  ) const;
+            deltaLJErrorHistogram_function_type deltaLJErrorHistogram_function_value( &::SireMM::GridFF::deltaLJErrorHistogram );
+            
+            GridFF_exposer.def( 
+                "deltaLJErrorHistogram"
+                , deltaLJErrorHistogram_function_value
+                , bp::return_value_policy<bp::clone_const_reference>() );
         
         }
         { //::SireMM::GridFF::ljCutoff
@@ -160,6 +205,48 @@ void register_GridFF_class(){
             GridFF_exposer.def( 
                 "spacing"
                 , spacing_function_value );
+        
+        }
+        { //::SireMM::GridFF::totalCoulombErrorHistogram
+        
+            typedef ::SireMaths::Histogram const & ( ::SireMM::GridFF::*totalCoulombErrorHistogram_function_type )(  ) const;
+            totalCoulombErrorHistogram_function_type totalCoulombErrorHistogram_function_value( &::SireMM::GridFF::totalCoulombErrorHistogram );
+            
+            GridFF_exposer.def( 
+                "totalCoulombErrorHistogram"
+                , totalCoulombErrorHistogram_function_value
+                , bp::return_value_policy<bp::clone_const_reference>() );
+        
+        }
+        { //::SireMM::GridFF::totalDeltaTimeSaved
+        
+            typedef double ( ::SireMM::GridFF::*totalDeltaTimeSaved_function_type )(  ) const;
+            totalDeltaTimeSaved_function_type totalDeltaTimeSaved_function_value( &::SireMM::GridFF::totalDeltaTimeSaved );
+            
+            GridFF_exposer.def( 
+                "totalDeltaTimeSaved"
+                , totalDeltaTimeSaved_function_value );
+        
+        }
+        { //::SireMM::GridFF::totalLJErrorHistogram
+        
+            typedef ::SireMaths::Histogram const & ( ::SireMM::GridFF::*totalLJErrorHistogram_function_type )(  ) const;
+            totalLJErrorHistogram_function_type totalLJErrorHistogram_function_value( &::SireMM::GridFF::totalLJErrorHistogram );
+            
+            GridFF_exposer.def( 
+                "totalLJErrorHistogram"
+                , totalLJErrorHistogram_function_value
+                , bp::return_value_policy<bp::clone_const_reference>() );
+        
+        }
+        { //::SireMM::GridFF::totalTotalTimeSaved
+        
+            typedef double ( ::SireMM::GridFF::*totalTotalTimeSaved_function_type )(  ) const;
+            totalTotalTimeSaved_function_type totalTotalTimeSaved_function_value( &::SireMM::GridFF::totalTotalTimeSaved );
+            
+            GridFF_exposer.def( 
+                "totalTotalTimeSaved"
+                , totalTotalTimeSaved_function_value );
         
         }
         { //::SireMM::GridFF::typeName
