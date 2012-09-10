c76303ee53dad61c276d653241e8a30833c85c5b
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-09-10 14:09:13 +0000

    Refining the OpenMM CMake stuff so that it works on Chris's laptop

--- corelib/branches/users/nividic/build/cmake/FindOpenMM.cmake
+++ corelib/branches/users/nividic/build/cmake/FindOpenMM.cmake
@@ -66,42 +66,47 @@ if(NOT IS_DIRECTORY ${OpenMM_ROOT_DIR})
 endif()
 
 if(NOT IS_DIRECTORY ${OpenMM_ROOT_DIR})
-    message(FATAL_ERROR "Could not find OpenMM! Set the OPENMM_ROOT_DIR environment "
-    "variable to contain the path of the OpenMM installation.")
+    message(STATUS "Could not find OpenMM! Set the OPENMM_ROOT_DIR environment "
+    "variable to contain the path of the OpenMM installation if you want to use it.")
 endif()
 
 if(NOT IS_DIRECTORY ${OpenMM_LIBRARY_DIR})
-    message(FATAL_ERROR "Can't find OpenMM libraries. Check your OpenMM installation!")
+    message(STATUS "Could not find OpenMM! Set the OPENMM_ROOT_DIR environment "
+    "variable to contain the path of the OpenMM installation if you want to use it.")
     set (OpenMM_FOUND 0)
 else()
     set (OpenMM_FOUND 1)
 endif()
 
-# now we can be sure that we have the library dir
-if(IS_DIRECTORY "${OpenMM_LIBRARY_DIR}/plugins")
-    get_filename_component(OpenMM_PLUGIN_DIR
-        "${OpenMM_LIBRARY_DIR}/plugins"
-        ABSOLUTE)
-    set(OpenMM_PLUGIN_DIR ${OpenMM_PLUGIN_DIR} CACHE PATH "OpenMM plugins directory")
-else()
-    message(WARNING "Could not detect the OpenMM plugin directory at the default location (${OpenMM_LIBRARY_DIR}/plugins)."
-            "Check your OpenMM installation or set the OPENMM_PLUGIN_DIR environment variable!")
+if ( ${OpenMM_FOUND} )
+  # now we can be sure that we have the library dir
+  if(IS_DIRECTORY "${OpenMM_LIBRARY_DIR}/plugins")
+      get_filename_component(OpenMM_PLUGIN_DIR
+          "${OpenMM_LIBRARY_DIR}/plugins"
+          ABSOLUTE)
+      set(OpenMM_PLUGIN_DIR ${OpenMM_PLUGIN_DIR} CACHE PATH "OpenMM plugins directory")
+  else()
+      message(WARNING "Could not detect the OpenMM plugin directory at the default location (${OpenMM_LIBRARY_DIR}/plugins)."
+              "Check your OpenMM installation or set the OPENMM_PLUGIN_DIR environment variable!")
+  endif()
+
+  if(NOT OpenMM_INCLUDE_DIR)
+      message(FATAL_ERROR "Can't find OpenMM includes. Check your OpenMM installation!")
+  endif()
+
+  set(OpenMM_ROOT_DIR ${OpenMM_ROOT_DIR} CACHE PATH "OpenMM installation directory")
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(OpenMM DEFAULT_MSG 
+                                      OpenMM_ROOT_DIR
+                                      OpenMM_LIBRARIES 
+                                      OpenMM_LIBRARY_DIR 
+                                      OpenMM_INCLUDE_DIR)
+ 
+  mark_as_advanced(OpenMM_INCLUDE_DIR
+      OpenMM_LIBRARIES
+      OpenMM_LIBRARIES_D
+      OpenMM_LIBRARY_DIR)
+
 endif()
 
-if(NOT OpenMM_INCLUDE_DIR)
-    message(FATAL_ERROR "Can't find OpenMM includes. Check your OpenMM installation!")
-endif()
-
-set(OpenMM_ROOT_DIR ${OpenMM_ROOT_DIR} CACHE PATH "OpenMM installation directory")
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenMM DEFAULT_MSG 
-                                    OpenMM_ROOT_DIR
-                                    OpenMM_LIBRARIES 
-                                    OpenMM_LIBRARY_DIR 
-                                    OpenMM_INCLUDE_DIR)
-
-mark_as_advanced(OpenMM_INCLUDE_DIR
-    OpenMM_LIBRARIES
-    OpenMM_LIBRARIES_D
-    OpenMM_LIBRARY_DIR)
