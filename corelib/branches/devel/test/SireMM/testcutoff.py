fffdab51452f84311b018f231561f6b059104928
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-10-15 11:05:54 +0000

    I've added an atomistic cutoff to the CLJ and softcore CLJ forcefields. This is tested in test/SireMM/testcutoff.py, which shows why using an atomistic cutoff is such a bad idea... (but it is worth including, so that such comparisons can be made, or if other long range correction schemes are applied on top of an atomistic cutoff)

--- corelib/branches/devel/test/SireMM/testcutoff.py
+++ corelib/branches/devel/test/SireMM/testcutoff.py
@@ -17,11 +17,15 @@ field_cljff = InterCLJFF("field_cutoff")
 field_cljff.setUseReactionField(True)
 field_cljff.setReactionFieldDielectric(78.0)
 
+atom_cljff = InterCLJFF("atomistic_cutoff")
+atom_cljff.setUseAtomisticCutoff(True)
+
 group_coul = group_cljff.components().coulomb()
 shift_coul = shift_cljff.components().coulomb()
 field_coul = field_cljff.components().coulomb()
+atom_coul = atom_cljff.components().coulomb()
 
-forcefields = [ group_cljff, shift_cljff, field_cljff ]
+forcefields = [ group_cljff, shift_cljff, field_cljff, atom_cljff ]
 
 switchfunc = HarmonicSwitchingFunction(10*angstrom, 9.5*angstrom)
 
@@ -44,7 +48,7 @@ system.setProperty("switchingFunction", switchfunc)
 printEnergies(system.energies())
  
 print "\nEnergy with respect to cutoff length\n"
-print "  Distance   Group    Shifted    ReactionField  "
+print "  Distance   Group    Shifted    ReactionField   Atomistic"
 
 for i in range(10,200,5):
     x = i*0.1
@@ -52,6 +56,6 @@ for i in range(10,200,5):
     switchfunc = HarmonicSwitchingFunction(x*angstrom, (x-0.5)*angstrom)
     system.setProperty("switchingFunction", switchfunc)
 
-    print "%12.8f  %12.8f  %12.8f  %12.8f" % (x, system.energy(group_coul).value(),
-              system.energy(shift_coul).value(), system.energy(field_coul).value())
-
+    print "%12.8f  %12.8f  %12.8f  %12.8f  %12.8f" % (x, system.energy(group_coul).value(),
+              system.energy(shift_coul).value(), system.energy(field_coul).value(),
+              system.energy(atom_coul).value())
