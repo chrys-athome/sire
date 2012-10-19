e7d23a0b9fe81116d23af675ed22b8577c215ac7
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2012-10-19 17:03:45 +0000

    I've finished writing the code to generate the zebedde input file from the user options. As far as I can tell, it is all ready to go :-)

--- software_project/conspire/trunk/job_classes/zebedde/go.sh
+++ software_project/conspire/trunk/job_classes/zebedde/go.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# first, write the input file based on the options
+# supplied by the user
+python write_cmdfile.py config run.dat
+
+# now run zebedde using the generated "run.dat" file
 ./zebedde run.dat > run.log
