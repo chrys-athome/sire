6a6c858323adbdbe0d2116ecb8259dc7223e6c54
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2013-08-16 10:28:42 +0000

    I've made a small change so that the streaming functions know that Soiree has been renamed to SireAnalysis

--- python2/branches/devel/Stream/__init__.py
+++ python2/branches/devel/Stream/__init__.py
@@ -11,22 +11,24 @@ import sys
 
 _pvt_load = load
 
-_pvt_modules = { "SireBase"   : "Sire.Base", 
-                 "SireCAS"    : "Sire.CAS",
-                 "SireCluster": "Sire.Cluster",
-                 "SireError"  : "Sire.Error",
-                 "SireFF"     : "Sire.FF",
-                 "SireID"     : "Sire.ID",
-                 "SireIO"     : "Sire.IO",
-                 "SireMM"     : "Sire.MM",
-                 "SireMaths"  : "Sire.Maths",
-                 "SireMol"    : "Sire.Mol",
-                 "SireMove"   : "Sire.Move",
-                 "SireSystem" : "Sire.System",
-                 "SireUnits"  : "Sire.Units",
-                 "SireVol"    : "Sire.Vol",
-                 "Squire"     : "Sire.Squire",
-                 "Soiree"     : "Sire.Soiree" }
+_pvt_modules = { "SireAnalysis" : "Sire.Analysis",
+                 "SireBase"     : "Sire.Base", 
+                 "SireCAS"      : "Sire.CAS",
+                 "SireCluster"  : "Sire.Cluster",
+                 "SireError"    : "Sire.Error",
+                 "SireFF"       : "Sire.FF",
+                 "SireID"       : "Sire.ID",
+                 "SireIO"       : "Sire.IO",
+                 "SireMM"       : "Sire.MM",
+                 "SireMaths"    : "Sire.Maths",
+                 "SireMol"      : "Sire.Mol",
+                 "SireMove"     : "Sire.Move",
+                 "SireSystem"   : "Sire.System",
+                 "SireUnits"    : "Sire.Units",
+                 "SireVol"      : "Sire.Vol",
+                 "Squire"       : "Sire.Squire",
+                 "Soiree"       : "Sire.Analysis"  # Soiree was renamed as Analysis 
+                }
 
 def _pvt_loadLibrary(lib):
 
