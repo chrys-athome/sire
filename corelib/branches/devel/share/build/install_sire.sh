e5805b420404b5bd403aa887a879f97ea0d1cb37
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2013-08-23 10:01:50 +0000

    I've cleaned up the Sire installation script and have removed version numbers from the sire executables

--- corelib/branches/devel/share/build/install_sire.sh
+++ corelib/branches/devel/share/build/install_sire.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "\n#######################################################"
+echo " "
+echo "#######################################################"
 echo "#      Where would you like to install Sire?"
 echo "#      Note that you can move Sire to any"
 echo "#      directory you want after installation"
 echo "#######################################################\n"
+echo " "
 
 echo "Full Path to Install directory ($HOME): "
 
@@ -20,14 +22,50 @@ else
     fi
 fi
 
-echo "\nInstalling Sire to $install_dir"
+echo " "
+echo "Installing Sire to $install_dir"
 mv sire.app $install_dir
 
-echo "\nSire is now installed in $install_dir/sire.app"
-echo "\nYou can run a Sire python script by typing $install_dir/sire.app/bin/python script.py"
-echo "\nAll Sire binaries are available in $install_dir/sire.app/bin"
-echo "\n#################################################"
-echo "#   Thanks for installing Sire"
-echo "#   Please get in touch if you have any problems"
-echo "#   (http://siremol.org)"
-echo "##################################################"
+if [ ! -d "$install_dir/sire.app" ]; then
+  echo " "
+  echo "************************************************"
+  echo "* WARNING - INSTALLATION FAILED"
+  echo "* PLEASE GET IN TOUCH WITH THE SIRE DEVELOPERS"
+  echo "* http://siremol.org"
+  echo "************************************************"
+  echo " "
+  exit -1
+fi
+
+echo " "
+echo "##############################################################################"
+echo "##"
+echo "## CONGRATULATIONS - SUCCESSFUL INSTALLATION"
+echo "##"
+echo "## Sire is installed in $install_dir/sire.app"
+echo "## You can run a Sire python script called script.py by typing"
+echo "## $install_dir/sire.app/bin/python script.py"
+echo "##"
+echo "## All Sire binaries are available in "
+echo "## $install_dir/sire.app/bin"
+echo "##"
+echo "## You can copy or move this directory if you want, and Sire will still work"
+echo "## Everything is contained in this directory, so you can delete Sire by"
+echo "## deleting this directory"
+echo "## (e.g. rm -rf $install_dir/sire.app)"
+echo "##"
+echo "## If you have never used Sire before, please take a look at the "
+echo "## Sire tutorial at http://siremol.org/tutorial"
+echo "##"
+echo "## Please also take a look at the examples at http://siremol.org/examples"
+echo "## for examples that you can download and use to learn Sire."
+echo "##"
+echo "## If you want to test this installation of Sire, please run"
+echo "## $install_dir/sire.app/bin/sire_test"
+echo "##"
+echo "## If you have any problems or questions please get in touch"
+echo "##"
+echo "##############################################################################"
+echo " "
+
+rm install_sire.sh
