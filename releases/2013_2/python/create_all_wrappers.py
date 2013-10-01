a19f808ef789cb48550312697545c4be88845518
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2013-10-01 10:54:31 +0000

    Tagging the 2013.2 release of the python wrappers

---
+++ releases/2013_2/python/create_all_wrappers.py
@@ -0,0 +1,32 @@
+
+import os
+
+dirs = [ "Analysis", \
+         "Base", \
+         "CAS", \
+         "Cluster", \
+         "DB", \
+         "FF", \
+         "ID", \
+         "IO", \
+         "MM", \
+         "Maths", \
+         "Mol", \
+         "Move", \
+         "Squire", \
+         "Stream", \
+         "System", \
+         "Units", \
+         "Vol" ]
+
+def create_wrappers(dir): 
+    os.chdir(dir)
+    os.system("python ../AutoGenerate/create_wrappers.py")
+    os.chdir("..")
+
+for dir in dirs:
+    create_wrappers(dir)
+
+os.chdir("Qt")
+os.system("python create_qt_wrappers.py")
+os.chdir("..")
