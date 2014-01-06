1064cb5d7add20f82f5db98b75188e80c9005b8f
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2014-01-06 15:00:03 +0000

    I've made changes to properly wrap the auto-conversion functions of NumberProperty and StringProperty, and have also replaced assert_equal with assert_almost_equal so that all of the tests now pass. I am ready to make a release :-)

--- python/branches/devel/Base/special_code.py
+++ python/branches/devel/Base/special_code.py
@@ -11,9 +11,15 @@ implicitly_convertible = [ ("QString", "SireBase::PropertyName"),
                            ("double", "SireBase::VariantProperty"),
                            ("QString", "SireBase::PropertyName"),
                            ("SireBase::Property", "SireBase::PropertyName"),
+                           ("QString", "SireBase::StringProperty"),
+                           ("SireBase::StringProperty", "QString"),
+                           ("double", "SireBase::NumberProperty"),
+                           ("int", "SireBase::NumberProperty"),
                            ("QHash<QString,SireBase::PropertyName>", "SireBase::PropertyMap") ] 
 
 def fixMB(mb):
     mb.add_declaration_code( "#include \"SireBase/propertymap.h\"" )
+    mb.add_declaration_code( "#include \"SireBase/stringproperty.h\"" )
+    mb.add_declaration_code( "#include \"SireBase/numberproperty.h\"" )
     mb.add_declaration_code( "#include <QString>" )
 
