b7d562109d90f73a17124a7a1862c6eab3b23a21
chryswoods@gmail.com <chryswoods@gmail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2014-03-24 15:23:24 +0000

    python wrapper help

--- python/branches/devel/Mol/special_code.py
+++ python/branches/devel/Mol/special_code.py
@@ -250,6 +250,7 @@ def fix_Mover(c):
     c.decls("mapInto").call_policies = call_policies.return_self()
     c.decls("translate").call_policies = call_policies.return_self()
     c.decls("rotate").call_policies = call_policies.return_self()
+    c.decls("transform").call_policies = call_policies.return_self()
     c.decls("changeFrame").call_policies = call_policies.return_self()
     c.decls("change").call_policies = call_policies.return_self()
     c.decls("set").call_policies = call_policies.return_self()
