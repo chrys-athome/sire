dd5834339a109dcbc3c2c323a6ff4fbecaa42363
julienmich@googlemail.com <julienmich@googlemail.com@361746d8-6d1a-0410-a2a1-f3f8340a69ac>
2014-09-18 09:04:22 +0000

    svn copy https://sire.googlecode.com/svn/python2/branches/users/julien https://sire.googlecode.com/svn/python2/tags/evotec2014pre

---
+++ python2/tags/evotec2014pre/julien/python/scripts/sire_test.py
@@ -0,0 +1,47 @@
+
+import os
+import sys
+
+import Sire.Config
+
+try:
+    import nose
+except:
+    print("Cannot import nose. Must install nose via sire.app/bin/install_package install nose")
+    sys.exit(-1)
+
+testdir = Sire.Config.test_directory
+
+old_cwd = os.getcwd()
+
+if len(sys.argv) > 1:
+    testdirs = sys.argv[1:]
+    sys.argv = [sys.argv[0]]
+else:
+    testdirs = os.listdir(testdir)
+
+failures = []
+
+for file in testdirs:
+    subdir = os.path.join(testdir, file)
+
+    if not file.startswith(".") and os.path.isdir(subdir):
+        print("\n\nRunning tests in directory %s..." % subdir)
+        print("############################################")
+        os.chdir(subdir)
+        success = nose.run()
+
+        if not success:
+            failures.append(file)
+
+        os.chdir(old_cwd)
+
+if len(failures) > 0:
+    print("\n\nWARNING: SOME OF THE TEST JOBS FAILED!!!")
+    print("#########################################")
+
+    for failure in failures:
+        print("One of more jobs in %s failed!" % failure)
+else:
+    print("\n\nHOORAY - ALL OF THE UNIT TESTS PASSED!!!")
+    print("\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/")
